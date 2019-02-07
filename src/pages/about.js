367ab1bed295b88d621e5d120b013ac5d68939a4
shredinger <caseylewiston@gmail.com>
2019-02-07 12:58:48 -0600

    Layout for About page, minor other fixes.

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,54 +16,65 @@ const About = () => (
       <div class="content-block">
         <div class="row">
           <div class="col-sm-auto mx-auto text-center">
-            <h2 class="section-heading text-white">Who We Are</h2> </div>
-            <div class="col-sm-auto mx-auto text-left">
+            <h2 class="section-heading text-white">Who We Are</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-auto mx-auto text-left">
+          <hr class="light my-4"></hr>
+          <p class="text-main mb-4">
+          Resurrection Roller Derby is a flat track roller derby league based out of Rohnert Park, CA. Since 2011 we've been bringing high quality competitive roller derby to Sonoma County. Our league is home to competitive players representing the North Bay throughout the country, recreational players scrimmaging at home and juniors developing their skills. We're a 501c(3) nonprofit created to promote the sport and provide opportunties for participation to the entire North Bay community. We're a member of the Women's Flat Track Roller Derby Association. <br></br>
+          </p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <img src="img/group1.jpg" class="w-50" /><br></br><br></br><br></br>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-white">Community Involvement</h2>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-auto mx-auto text-left">
             <hr class="light my-4"></hr>
             <p class="text-main mb-4">
-            Resurrection Roller Derby is a flat track roller derby league based out of Rohnert Park, CA. Since 2011 we've been bringing high quality competitive roller derby to Sonoma County. Our league is home to competitive players representing the North Bay throughout the country, recreational players scrimmaging at home and juniors developing their skills. We're a 501c(3) nonprofit created to promote the sport and provide opportunties for participation to the entire North Bay community. We're a member of the Women's Flat Track Roller Derby Association. <br></br>   </p> </div>
-
-            </div>
-            <div class="row">
-              <div class="col-sm-12 text-center">
-                <img src="img/group1.jpg" class="w-50" /><br></br><br></br><br></br>
-              </div></div>
-            </div>
-            <div class="row">
-              <div class="col-lg-12 text-center">
-                <h2 class="section-heading text-white">Community Involvement</h2>
-              </div>
-              <div class="col-sm-auto mx-auto text-left">
-              <hr class="light my-4"></hr>
-              <p class="text-main mb-4">
-              We take great pride in supporting our local community. Our contributions include volunteering, donating and advocating to spread awareness for important causes. At each of our games, a portion of proceeds are donated to a charitable cause. We also participate in blood drives, volunteer at local events and do what we can to make the community a better place.<br></br><br></br>
-              <strong>Resurrection Roller Girls' Charity Partners</strong><br></br>
-
-              <ul class="list-unstyled text-main mb-4">
-                <li>Girls On Track</li>
-                <li>The Living Room of Santa Rosa</li>
-                <li>Social Advocates For Youth</li>
-                <li>Redwood Empire Food Bank</li>
-                <li>American Red Cross</li>
-                <li>Council on Aging's Meals on Wheels</li>
-                <li>Verity</li>
-                <li>Committee on the Shelterless (COTS)</li>
-            </ul></p></div>
-            <div class="row"><p><br></br><br></br></p></div>
+            We take great pride in supporting our local community. Our contributions include volunteering, donating and advocating to spread awareness for important causes. At each of our games, a portion of proceeds are donated to a charitable cause. We also participate in blood drives, volunteer at local events and do what we can to make the community a better place.
+            <br></br><br></br>
+            <strong>Resurrection Roller Girls' Charity Partners</strong><br></br>
+            <ul class="list-unstyled text-main mb-4">
+              <li>Girls On Track</li>
+              <li>The Living Room of Santa Rosa</li>
+              <li>Social Advocates For Youth</li>
+              <li>Redwood Empire Food Bank</li>
+              <li>American Red Cross</li>
+              <li>Council on Aging's Meals on Wheels</li>
+              <li>Verity</li>
+              <li>Committee on the Shelterless (COTS)</li>
+            </ul>
+            </p>
           </div>
-          <div class="row">
-            <div class="col-sm-6 text-center">
-              <img src="img/blood.jpg" class="img-fluid" />
-            </div>
-            <div class="col-sm-6 text-center">
-              <img src="img/cotatitarod.jpg" class="img-fluid" />
-            </div>
+        </div>
+        <div class="row"><p><br></br><br></br></p></div>
+
+        <div class="row">
+          <div class="col-sm-6 text-center">
+            <img src={require('../img/blood.jpg')} class="img-fluid" />
           </div>
+          <div class="col-sm-6 text-center">
+            <img src={require('../img/cotatitarod.jpg')} class="img-fluid" />
+          </div>
+        </div>
         <div class="row">
           <div class="col-sm-auto text-left">
             <p class="text-main mb4"><br></br><br></br><br></br>If your nonprofit charity would like to be considered as a sponsored benefit, or you would like to discuss volunteer and outreach opportunities with our league, please send us an <a href="mailto:info@rrgderby.org">email.</a></p>
           </div>
         </div>
-    
+      </div>
       </section>
 
   </Layout>
