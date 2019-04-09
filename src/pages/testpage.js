8433e26a7dfaf928b7b61bf9515d09c62f0bc29b
shredinger <caseylewiston@gmail.com>
2019-04-09 16:21:39 -0500

    Starting calendar build out

--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import '../vendor/bootstrap/css/bootstrap.css'
 import '../css/rrderby.css'
 import SEO from "../components/seo"
+import GetEvents from "../components/getevents"
 
 
 
@@ -17,6 +18,7 @@ const Testpage = () => (
           <h2 className="section-heading text-white">Juniors</h2> </div>
           <div className="col-sm-auto mx-auto text-left">
           <hr className="light my-4" / >
+          <GetEvents />
           </div>
           </div>
 
