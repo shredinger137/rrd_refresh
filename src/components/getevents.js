d50be92f8e58af3ea08f3231e7f19c03aa04ed73
shredinger <caseylewiston@gmail.com>
2019-04-10 18:17:03 -0500

    Data now pulls, filters. Still need link parsing and other event types.

--- src/components/getevents.js
+++ src/components/getevents.js
@@ -7,7 +7,7 @@ export default class GetEvents extends React.Component {
   state = {}
   componentDidMount() {
 
-    var calURL = "https://www.googleapis.com/calendar/v3/calendars/admin@rrgderby.org/events?key=";
+    var calURL = "https://www.googleapis.com/calendar/v3/calendars/admin@rrgderby.org/events?key=" + process.env.ENV_CAL_KEY;
     var request =  new XMLHttpRequest();
     var eventData, status = false;
     const setState = this.setState.bind(this);
@@ -32,25 +32,68 @@ export default class GetEvents extends React.Component {
   testFilter(data){
 
     var results = "";
+    var holdArr = [];
+    var resultsArray = [];
+    var date;
+
     for(var i = 0; i <= Object.keys(data).length; i++){
       if(data && data[i] && data[i]['summary']){
         if(data[i]['summary'].includes("Game")){
-        console.log(data[i]['summary'])
-        results = results + "<br /> " + data[i]['summary'];
+          if(data[i]['start']['dateTime']){
+            var parsedDate = new Date(data[i]['start']['dateTime']);
+            var current = new Date();
+              if(current.getYear() == parsedDate.getYear()){
+                var mm = parsedDate.getMonth() + 1;
+                var dd = parsedDate.getDate();
+                date = mm + "/" + dd;
+                var team = data[i]['summary'].substring(14, data[i]['summary'].length);
+                if(data[i]['summary'].includes("Home")){
+                  var place = "Home";
+                } else {
+                  var place = "Away";
+                }
+                //TODO: This doesn't seem to be finding matches, but is correctly seeing the data.
+                if(data[i]['description']){
+                  var string = "";
+                  var matches = string.match(/\bhttps?:\/\/\S+/gi);
+                  console.log("searched");
+                  console.log(matches);
+                  console.log(data[i]['description'])
+                }
+                else{
+                  var link = "null";
+                }
+
+
+                holdArr = [team, place, data[i]['location'], date, matches];
+                resultsArray.push(holdArr);
+              }
+            }
+
+          results = results + "<br /> " + data[i]['summary'];
       }}
       }
-    return results;
+    console.log(resultsArray);
+    return resultsArray;
   }
 
 
   render() {
     return (
     <div>
+    <table className="events-table">
     {!this.state ?
               <div>{console.log("Loading")}</div>
           :
-              <div>{this.testFilter(this.state)}</div>
+              <div>{(this.testFilter(this.state)).map((event) =>
+                <tr>
+                <td>{event[0]}</td>
+                <td>{event[1]}</td>
+                <td>{event[3]}</td>
+                </tr>
+              )}</div>
           }
+    </table>
     </div>
     );
   }
--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -9,23 +9,14 @@ import GetEvents from "../components/getevents"
 
 const Testpage = () => (
 
-  <div>
-    <SEO title="Resurrection Roller Derby" keywords={[`roller derby`, `sonoma county`, `rohnert park`, 'north bay', 'resurrection', 'sports']} />
     <section>
-      <div className="content-block">
-      <div className="row">
-        <div className="col-sm-auto mx-auto text-center">
-          <h2 className="section-heading text-white">Juniors</h2> </div>
+
           <div className="col-sm-auto mx-auto text-left">
-          <hr className="light my-4" / >
           <GetEvents />
           </div>
-          </div>
 
-        </div>
   </section>
 
-  </div>
 
 )
 
