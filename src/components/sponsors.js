b54c10a5d13338f2d3c82ac91dbf72724ad91aa0
Casey Lewiston <caseylewiston@gmail.com>
2019-04-13 23:16:49 -0500

    Sponsors page - list is now built from Contentful content type.

---
+++ src/components/sponsors.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import './sponsors.css'
+
+class SponsorList extends React.Component {
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query SponsorListQuery {
+              allContentfulSponsors {
+                edges {
+                  node {
+                    name
+                    id
+                    link
+                    logo {
+                      id
+                      file {
+                        url
+                        fileName
+                        contentType
+                      }
+                    }
+                  }
+                }
+              }
+            }
+      `   }
+
+        render={data => (
+          <>
+
+
+                  <div className="sponsors">
+                    <ul className="sponsor_list">
+                      {data.allContentfulSponsors.edges.map(({ node }) => {
+                        return (
+
+                            <li key={node.id}>
+                            <a href={node.link} target="sponsors_new" key={node.link}>
+                            <img src={node.logo.file.url} className="img-fluid text-center" alt={node.name} key={node.logo.fileName}/></a>
+                            <br/>
+                            <p key={node.name}>{node.name}</p>
+
+                            </li>
+
+                        );
+                      })}
+                    </ul>
+                  </div>
+
+
+          </>
+        )}
+      />
+    );
+  }
+}
+
+export default SponsorList;
--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -5,32 +5,32 @@ import '../css/rrderby.css'
 import SEO from "../components/seo"
 import SponsorList from "../components/sponsors"
 
-const Sponsors = () => (
+const TestPage = () => (
 
   <Layout>
     <SEO title="Resurrection Roller Derby" keywords={[`roller derby`, `sonoma county`, `rohnert park`, 'north bay', 'resurrection', 'sports']} />
-      <section id="sponsors" className="secondSection">
-      <div className="content-block">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-auto mx-auto text-center">
-              <h1 className="section-heading text-white">Our Sponsors</h1>
-              <div className="col-sm-auto mx-auto text-left">
-                <hr className="light my-4" />
-              </div>
+    <section id="sponsors" className="secondSection">
+    <div className="content-block">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-auto mx-auto text-center">
+            <h1 className="section-heading text-white">Our Sponsors</h1>
+            <div className="col-sm-auto mx-auto text-left">
+              <hr className="light my-4" />
             </div>
           </div>
 
+
           <SponsorList />
 
-        </div>
 
         </div>
-
+        </div>
+        </div>
       </section>
 
   </Layout>
 
 )
 
-export default Sponsors
+export default TestPage
